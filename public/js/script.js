84ff10fc24e5c5e3336f3c1301dad6f2d214943b
João Lucas <joaolucascastro624@gmail.com>
2025-10-25 17:09:49 -0300

    Modal pronto

--- public/js/script.js
+++ public/js/script.js
@@ -2,3 +2,7 @@ function abrirMenu(menuMostra, menuEsconde) {
     document.getElementById(menuMostra).style.display = "flex";
     document.getElementById(menuEsconde).style.display = "none";
 }
+function fechaMenu(menuMostra, menuEsconde) {
+    document.getElementById(menuMostra).style.display = "none";
+    document.getElementById(menuEsconde).style.display = "flex";
+}
