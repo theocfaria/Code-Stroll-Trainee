cca8fae605bffa4af4d7e75319523072e1b43965
DanielQueiroz <daniel.queiroz@estudante.ufjf.br>
2025-10-28 11:10:00 -0300

    Correção carrossel

--- public/js/landing-page.js
+++ public/js/landing-page.js
@@ -1,7 +1,7 @@
 // Passando elementos HTML para variaveis JavaScript
-const slider = document.querySelector('.slider');
-const sliderContent = document.querySelector('.slider-conteudo');
-const radioAuto = document.querySelector('.radio-auto');
+const slider = document.querySelector(".slider");
+const sliderContent = document.querySelector(".slider-conteudo");
+const radioAuto = document.querySelector(".radio-auto");
 const leftArrow = document.getElementById("seta-esquerda");
 const rightArrow = document.getElementById("seta-direita");
 
@@ -13,94 +13,85 @@ let autoSlideInterval;
 
 // Criação/Organização do Carrossel
 function updateCarrossel() {
+  const sliderWidth = slider.offsetWidth;
+  const itemWidth = sliderContent.children[0].getBoundingClientRect().width;
 
- const sliderWidth = slider.offsetWidth;
- const itemWidth = sliderContent.children[0].getBoundingClientRect().width;
+  itemsPerView = sliderWidth / itemWidth;
+  totalPages = Math.ceil(
+    sliderContent.children.length / itemsPerView -
+      2 * (sliderWidth / itemWidth / 100)
+  );
 
- itemsPerView = (sliderWidth / itemWidth)
- totalPages = Math.ceil((sliderContent.children.length / itemsPerView) -2* ((sliderWidth / itemWidth)/100))
-
- createRadioLabel();
- updateRadioLabel();
+  createRadioLabel();
+  updateRadioLabel();
 }
 
 // RADIO LABEL
 function createRadioLabel() {
-
- radioAuto.innerHTML = "";
- for (let i = 0; i < totalPages; i++) {
-
-  const label = document.createElement('div');
-  label.classList.add('radio-label');
-  if (i === 0) {
-
-   label.classList.add('active');
+  radioAuto.innerHTML = "";
+  for (let i = 0; i < totalPages; i++) {
+    const label = document.createElement("div");
+    label.classList.add("radio-label");
+    if (i === 0) {
+      label.classList.add("active");
+    }
+    label.addEventListener("click", () => {
+      currentPage = i;
+      scrollToPage();
+    });
+    radioAuto.appendChild(label);
   }
-  label.addEventListener('click', () => {
-   currentPage = i;
-   scrollToPage();
-  })
-  radioAuto.appendChild(label);
- }
 }
 
 function updateRadioLabel() {
-
- const labels = document.querySelectorAll('.radio-label');
- label.forEach((l, i) => {
-  l.classList.toggle('active', i === currentPage);
- });
+  const labels = document.querySelectorAll(".radio-label");
+  labels.forEach((l, i) => {
+    l.classList.toggle("active", i === currentPage);
+  });
 }
 
 // MOVIMENTAÇÃO
 function scrollToPage() {
-
- const newPosition = slider.offsetWidth * currentPage;
- sliderContent.scrollTo({ left: newPosition, behavior: 'smooth' });
- updateRadioLabel();
- resetAutoSlide();
+  const newPosition = slider.offsetWidth * currentPage;
+  sliderContent.scrollTo({ left: newPosition, behavior: "smooth" });
+  updateRadioLabel();
+  resetAutoSlide();
 }
 
 function moveleft() {
-
- currentPage--;
- if (currentPage < 0) 
- {
-  currentPage = totalPages - 1;
- }
- scrollToPage();
- resetAutoSlide();
+  currentPage--;
+  if (currentPage < 0) {
+    currentPage = totalPages - 1;
+  }
+  scrollToPage();
+  resetAutoSlide();
 }
 
 function moveRight() {
-
- currentPage++;
- if (currentPage >= totalPages) 
- {
-  currentPage = 0;
- }
- scrollToPage();
- resetAutoSlide();
+  currentPage++;
+  if (currentPage >= totalPages) {
+    currentPage = 0;
+  }
+  scrollToPage();
+  resetAutoSlide();
 }
 
 // SLIDE AUTOMATICO
 function startAutoSlide() {
-
- autoSlideInterval = setInterval(() => {
-  moveRight();
- }, 4000)
+  autoSlideInterval = setInterval(() => {
+    moveRight();
+  }, 4000);
 }
 
-function resetAutoSlide() 
-{
- clearInterval(autoSlideInterval);
- startAutoSlide();
+function resetAutoSlide() {
+  clearInterval(autoSlideInterval);
+  startAutoSlide();
 }
 
 // EVENTOS
-leftArrow.addEventListener('click', moveleft);
-rightArrow.addEventListener('click', moveRight);
-window.addEventListener('resize', updateCarrossel);
+leftArrow.addEventListener("click", moveleft);
+rightArrow.addEventListener("click", moveRight);
+window.addEventListener("resize", updateCarrossel);
 
 // CHAMA INÍCIO DAS FUNÇÕES
 updateCarrossel();
